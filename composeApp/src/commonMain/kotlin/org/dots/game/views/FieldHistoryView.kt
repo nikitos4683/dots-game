39245d9c213700269cf95737fe56909bec0c38fb
Ivan Kochurkin <kvanttt@gmail.com>
2025-01-11 18:42:29 +0100

    Make `FieldHistoryView` scrollable

--- composeApp/src/commonMain/kotlin/org/dots/game/views/FieldHistoryView.kt
+++ composeApp/src/commonMain/kotlin/org/dots/game/views/FieldHistoryView.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,6 +23,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import org.dots.game.UiSettings
@@ -49,7 +52,9 @@ fun FieldHistoryView(
     onChangeCurrentNode: () -> Unit
 ) {
     val requester = remember { FocusRequester() }
-    Box(Modifier.size(300.dp, 300.dp).padding(stepSize)
+    Box(Modifier.size(300.dp, 200.dp)
+        .horizontalScroll(rememberScrollState())
+        .verticalScroll(rememberScrollState())
         .focusRequester(requester)
         .focusable()
         .onKeyEvent { keyEvent ->
@@ -60,11 +65,38 @@ fun FieldHistoryView(
             }
         }
     ) {
-        ConnectionsAndNodes(fieldHistory, fieldHistoryViewData.fieldHistoryElements, onChangeCurrentNode, uiSettings)
-        CurrentNode(currentNode, fieldHistoryViewData.nodeToIndexMap)
+        Box(Modifier.size(fieldHistoryViewData.size)) {
+            ConnectionsAndNodes(
+                fieldHistory,
+                fieldHistoryViewData.fieldHistoryElements,
+                onChangeCurrentNode,
+                uiSettings
+            )
+            CurrentNode(currentNode, fieldHistoryViewData)
+        }
     }
     LaunchedEffect(fieldHistory) {
         requester.requestFocus()
+    }
+}
+
+class FieldHistoryViewData(fieldHistory: FieldHistory) {
+    val fieldHistoryElements: FieldHistoryElements = fieldHistory.getHistoryElements(mainBranchIsAlwaysStraight = true)
+
+    val size: DpSize = DpSize(
+        stepSize * fieldHistoryElements.size + nodeSize,
+        stepSize * fieldHistoryElements.maxOf { yLine -> yLine.size } + nodeSize
+    )
+
+    val nodeToIndexMap: Map<Node, Pair<Int, Int>> by lazy {
+        buildMap {
+            for (xIndex in fieldHistoryElements.indices) {
+                for ((yIndex, element) in fieldHistoryElements[xIndex].withIndex()) {
+                    val node = (element as? NodeHistoryElement)?.node ?: continue
+                    this[node] = xIndex to yIndex
+                }
+            }
+        }
     }
 }
 
@@ -76,10 +108,10 @@ private fun ConnectionsAndNodes(
     uiSettings: UiSettings
 ) {
     for (xIndex in fieldHistoryElements.indices) {
-        for ((yIndex, element) in fieldHistoryElements[xIndex].withIndex()) {
+        val offsetX = stepSize * xIndex
 
-            val centerOffsetX = stepSize * xIndex
-            val centerOffsetY = stepSize * yIndex
+        for ((yIndex, element) in fieldHistoryElements[xIndex].withIndex()) {
+            val offsetY = stepSize * yIndex
 
             when (element) {
                 is NodeHistoryElement -> {
@@ -97,7 +129,7 @@ private fun ConnectionsAndNodes(
                         // Render horizontal connection line
                         Box(
                             Modifier
-                                .offset(stepSize * (xIndex - 1), centerOffsetY - lineThickness / 2)
+                                .offset(stepSize * (xIndex - 1) + nodeRadius, offsetY - lineThickness / 2 + nodeRadius)
                                 .size(stepSize, lineThickness)
                                 .background(lineColor)
                                 .zIndex(linesZIndex)
@@ -107,7 +139,7 @@ private fun ConnectionsAndNodes(
                     // Render node
                     Box(
                         Modifier
-                            .offset(centerOffsetX - nodeRadius, centerOffsetY - nodeRadius)
+                            .offset(offsetX, offsetY)
                             .size(nodeSize, nodeSize)
                             .clip(CircleShape)
                             .background(color)
@@ -128,7 +160,7 @@ private fun ConnectionsAndNodes(
                     // Render vertical connection line
                     Box(
                         Modifier
-                            .offset(stepSize * xIndex - lineThickness / 2, centerOffsetY - stepSize)
+                            .offset(stepSize * xIndex - lineThickness / 2 + nodeRadius, offsetY - stepSize + nodeRadius)
                             .size(lineThickness, stepSize)
                             .background(lineColor)
                             .zIndex(linesZIndex)
@@ -142,18 +174,14 @@ private fun ConnectionsAndNodes(
 }
 
 @Composable
-private fun CurrentNode(currentNode: Node?, nodeToIndexMap: Map<Node, Pair<Int, Int>>) {
+private fun CurrentNode(currentNode: Node?, fieldHistoryViewData: FieldHistoryViewData) {
     if (currentNode == null) return
-    val (xIndex, yIndex) = nodeToIndexMap.getValue(currentNode)
-
-    val centerOffsetX = stepSize * xIndex - nodeRadius
-    val centerOffsetY = stepSize * yIndex - nodeRadius
-    val size = nodeRadius * 2
+    val (xIndex, yIndex) = fieldHistoryViewData.nodeToIndexMap.getValue(currentNode)
 
     Box(
         Modifier
-            .offset(centerOffsetX, centerOffsetY)
-            .size(size, size)
+            .offset(stepSize * xIndex, stepSize * yIndex)
+            .size(nodeSize, nodeSize)
             .border(1.dp, selectedNodeRectColor)
             .zIndex(selectedNodeZIndex)
     )
