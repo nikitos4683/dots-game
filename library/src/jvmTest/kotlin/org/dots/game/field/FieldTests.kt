092a48f7d942fd6e6ffa69ef864ee8fe2c83a452
Ivan Kochurkin <kvanttt@gmail.com>
2025-07-13 16:35:06 +0200

    [Test] Introduce `testFieldWithTransformsAndRollback`

--- library/src/jvmTest/kotlin/org/dots/game/field/FieldTests.kt
+++ library/src/jvmTest/kotlin/org/dots/game/field/FieldTests.kt
@@ -3,7 +3,10 @@ package org.dots.game.field
 import org.dots.game.core.BaseMode
 import org.dots.game.core.Field
 import org.dots.game.core.MoveInfo
+import org.dots.game.core.Position
 import org.dots.game.core.Rules
+import org.dots.game.core.TransformType
+import org.dots.game.core.transform
 import org.dots.game.core.unmakeAllMovesAndCheck
 import org.dots.game.dump.FieldParser
 import kotlin.test.fail
@@ -15,6 +18,29 @@ abstract class FieldTests {
     open val initialMoves: List<MoveInfo> = listOf()
 
     fun initRules(width: Int, height: Int): Rules =  Rules(width, height, captureByBorder, baseMode, suicideAllowed, initialMoves)
+
+    fun testFieldWithTransformsAndRollback(fieldData: String, check: (field: Field, transformFunc: (Position) -> Position) -> Unit) {
+        val originalField = FieldParser.parseAndConvert(fieldData, initializeRules = { width, height -> initRules(width, height) })
+
+        fun check(transformType: TransformType?) {
+            val field = transformType?.let { originalField.transform(transformType) } ?: originalField.clone()
+            check(field) {
+                if (transformType != null) {
+                    it.transform(transformType, originalField.realWidth, originalField.realHeight)
+                } else {
+                    it
+                }
+            }
+            field.unmakeAllMovesAndCheck { fail(it) }
+        }
+
+        check(transformType = null)
+        check(transformType = TransformType.RotateCw90)
+        check(transformType = TransformType.Rotate180)
+        check(transformType = TransformType.RotateCw270)
+        check(transformType = TransformType.FlipHorizontal)
+        check(transformType = TransformType.FlipVertical)
+    }
 
     fun testFieldWithRollback(fieldData: String, check: (Field) -> Unit) {
         with (FieldParser.parseAndConvert(fieldData, initializeRules = { width, height -> initRules(width, height) })) {
