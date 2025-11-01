c9126bab270c29df62cb0c652c4aae755c9e6042
Ivan Kochurkin <kvanttt@gmail.com>
2025-11-01 12:31:32 +0100

    [Wasm] Implement state saving on app closing
    fix #52

--- composeApp/src/wasmJsMain/kotlin/org/dots/game/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/dots/game/main.kt
@@ -1,12 +1,27 @@
 package org.dots.game
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
+import org.dots.game.core.Games
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        val currentGameSettings by remember { mutableStateOf(loadCurrentGameSettings()) }
+        var games: Games? by remember { mutableStateOf(null) }
+
+        window.addEventListener("beforeunload", { event ->
+            saveCurrentGameSettings(currentGameSettings, games)
+        })
+
+        App(currentGameSettings) {
+            games = it
+        }
     }
 }
